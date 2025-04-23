8632b32b6f35ff5eebed7ad70b7df8cc1fe40038
Paras <officialkalura@gmail.com>
2025-04-23 10:24:37 +0530

    commit

---
+++ src/components/Fourground.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Fourground = () => {
+  return (
+    
+        <div className=' fixed  top-0 left-0 z-[3] w-full h-screen' >
+
+
+            <div className='w-40 h-50 rounded  bg-sky-200' >Card1 </div>
+
+</div>
+
+    
+  )
+}
+
+export default Fourground
+
